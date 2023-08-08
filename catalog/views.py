9a11a8f120fe89e67a78481d66bb7c79bb59c1d1
BasilZap <zapr9@yandex.ru>
2023-08-08 23:18:58 +0300

    Выполнены задания №3 и №4

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "catalog/index.html")
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Имя - {name}'
+              f'Телефон - {phone}'
+              f'Сообщение - {message}')
+
+    return render(request, "catalog/contact.html")
